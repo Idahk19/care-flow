baf432328ba07e9ae3f65a487cfd9a255d3b007c
Idahk19 <idahkarwitha9@gmail.com>
2026-09-07 20:27:08 +0300

    Load environment variables and update SECRET_KEY configuration in settings.py

--- careflow/settings.py
+++ careflow/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-na#s08-y*tm=9^2qk-=e3)quaf3#6x$!1(uf^lzmarj6rj)yh%'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+AUTH_USER_MODEL = 'accounts.User'
 
 ALLOWED_HOSTS = []
 
