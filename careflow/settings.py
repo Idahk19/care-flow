502c49f77418a1b2da18fff63f49709dba122b80
Idahk19 <idahkarwitha9@gmail.com>
2026-09-07 19:49:41 +0300

    Refactor project structure by removing unused app files and adding new accounts, appointments, hospital, and queue_management apps with initial configurations.

--- careflow/settings.py
+++ careflow/settings.py
@@ -38,9 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+
+    'accounts',
+    'hospital',
+    'appointments',
+    'queue_management',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
