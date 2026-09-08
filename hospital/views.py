7d1f58a5db3e6e2de97488dfe26d0a9322d71894
Idahk19 <idahkarwitha9@gmail.com>
2026-09-08 14:56:33 +0300

    Add rest_framework.authtoken to installed apps and remove permission classes from Doctor and Service viewsets

--- hospital/views.py
+++ hospital/views.py
@@ -8,10 +8,10 @@ from .serializers import DoctorSerializer, ServiceSerializer
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    
