282503a73c5efb88f34d377b9ddc51ac9847fc53
Idahk19 <idahkarwitha9@gmail.com>
2026-09-08 09:24:20 +0300

    Add registration and login serializers for user authentication

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,14 +2,52 @@ from rest_framework import serializers
 from .models import User
 
 
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'role'
+        ]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            password=validated_data['password'],
+            role=validated_data.get('role', 'PATIENT')
+        )
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        write_only=True
+    )
+
+
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = [
             'id',
+            'email',
             'first_name',
             'last_name',
-            'email',
-            'phone_number',
-            'role',
+            'role'
         ]
+        read_only_fields = ['id', 'email']
