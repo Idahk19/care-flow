5d7f4fd3bb5288c6164259bdd556501ebfa970ee
Idahk19 <idahkarwitha9@gmail.com>
2026-09-10 11:24:47 +0300

    Add DepartmentSerializer and update Doctor and Service serializers to include department_name

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -1,14 +1,55 @@
 from rest_framework import serializers
-from .models import Doctor, Service
+from .models import Department, Doctor, Service
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = [
+            'id',
+            'name',
+            'description',
+            'is_active',
+        ]
+        read_only_fields = ['id']
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    department_name = serializers.CharField(
+        source='department.name',
+        read_only=True
+    )
+
     class Meta:
         model = Doctor
-        fields = '__all__'
+        fields = [
+            'id',
+            'department',
+            'department_name',
+            'first_name',
+            'last_name',
+            'specialization',
+            'phone_number',
+            'email',
+            'is_active',
+        ]
+        read_only_fields = ['id', 'department_name']
 
 
 class ServiceSerializer(serializers.ModelSerializer):
+    department_name = serializers.CharField(
+        source='department.name',
+        read_only=True
+    )
+
     class Meta:
         model = Service
-        fields = '__all__'
+        fields = [
+            'id',
+            'department',
+            'department_name',
+            'name',
+            'description',
+            'is_active',
+        ]
+        read_only_fields = ['id', 'department_name']
