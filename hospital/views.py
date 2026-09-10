ff95c056d47b90394c1e9fae4de542d2d903313f
Idahk19 <idahkarwitha9@gmail.com>
2026-09-10 11:25:11 +0300

    Enforce admin permissions for Doctor and Service viewsets

--- hospital/views.py
+++ hospital/views.py
@@ -1,17 +1,27 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser
 
-from .models import Doctor, Service
-from .serializers import DoctorSerializer, ServiceSerializer
+from .models import Department, Doctor, Service
+from .serializers import (
+    DepartmentSerializer,
+    DoctorSerializer,
+    ServiceSerializer,
+)
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsAdminUser]
 
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    
+    permission_classes = [IsAdminUser]
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
-    
+    permission_classes = [IsAdminUser]
